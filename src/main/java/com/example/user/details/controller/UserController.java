5a7e1aec81f75e664d05ff1c4590d22c0d2aca6c
magesh1 <mageshcruz@gmail.com>
2025-02-11 23:56:17 +0530

    added swagger docs

--- src/main/java/com/example/user/details/controller/UserController.java
+++ src/main/java/com/example/user/details/controller/UserController.java
@@ -2,6 +2,11 @@ package com.example.user.details.controller;
 
 import com.example.user.details.model.UserDetails;
 import com.example.user.details.service.UserService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
@@ -24,12 +29,24 @@ public class UserController {
         this.userService = userService;
     }
 
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserDetails.class))}),
+            @ApiResponse(responseCode = "409", description = "email already exists", content = @Content),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = @Content)
+    })
+    @Tag(name = "post", description = "create user api")
     @PostMapping("/user")
     public ResponseEntity<String> createUser(@RequestBody UserDetails userDetails) {
         userService.createUser(userDetails);
         return ResponseEntity.status(HttpStatus.CREATED).body("message: success");
     }
 
+    @ApiResponses({
+            @ApiResponse(responseCode = "404", description = "user not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = @Content)
+    })
+    @Tag(name = "get", description = "get user details where you can sort the data or get response by pagination")
     @GetMapping("/user")
     public ResponseEntity<?> listUser(@RequestParam(defaultValue = "createdAt") String sortBy,
                                       @RequestParam(defaultValue = "desc") String orderBy,
